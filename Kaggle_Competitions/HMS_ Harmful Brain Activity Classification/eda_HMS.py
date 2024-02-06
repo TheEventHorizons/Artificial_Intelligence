5b67b0ae813403a4b0a1c3f8c9a02b99422b807d
TheEventHorizons <beyondtheeventhorizons@gmail.com>
2024-02-06 16:16:32 +0100

    Update eda_HMS.py

--- Kaggle_Competitions/HMS_ Harmful Brain Activity Classification/eda_HMS.py
+++ Kaggle_Competitions/HMS_ Harmful Brain Activity Classification/eda_HMS.py
@@ -136,6 +136,145 @@ plt.show()
 ########## Significance of Variables ##########
 
 
+
+
+# label_id: Nothing to say since it's a unique number representing each cases in the dataset
+
+
+
+
+
+# eeg_id, eeg_sub_id, eeg_label_offset_seconds: 
+
+# There are 17089 different eeg_id. 
+# Each eeg_id contains eeg_sub_id which can go from 0 to 742 associated to a eeg_label_offset_seconds which goes from 0.0 to 3372.0. Thus the occurency of a eeg_id goes from 1 to 743.
+# A eeg_id is associated to a unique spectrogram_id.
+
+
+# Count the number of unique eeg_id
+df_count_eeg_id = df['eeg_id'].value_counts().reset_index()
+df_count_eeg_id.columns = ['eeg_id','Count']
+print(df_count_eeg_id)
+
+'''
+# Display if in a figure
+plt.figure(figsize=(12,8))
+plt.bar(df_count_eeg_id.index, df_count_eeg_id['Count'], color=colors, zorder=2)
+plt.xlabel('eeg_id')
+plt.ylabel('Count')
+plt.title('Distribution of eeg_id')
+plt.legend()
+plt.grid(ls='--')
+plt.show()
+'''
+
+# Display the dataframe corresponding to the eeg_id which appears the most 'eeg_id'=2259539799
+df_max_eeg_id = df[df['eeg_id']==df_count_eeg_id.loc[df_count_eeg_id['Count'].idxmax(),'eeg_id']]
+print(df_max_eeg_id)
+
+# Display the different result of expert consensus for this eeg_id
+print(df_max_eeg_id['expert_consensus'].unique())
+
+# Display the dataframe corresponding to the eeg_id which appears the least 'eeg_id'=98046913 (one of them)
+df_min_eeg_id = df[df['eeg_id']==df_count_eeg_id.loc[df_count_eeg_id['Count'].idxmin(),'eeg_id']]
+print(df_min_eeg_id)
+
+# Display the different result of expert consensus for this eeg_id
+print(df_min_eeg_id['expert_consensus'].unique())
+
+# Display the min-max eeg_label_offset_seconds
+print(df['eeg_label_offset_seconds'].min())
+print(df['eeg_label_offset_seconds'].max())
+
+
+# Count the unique spectrogram_ids for each eeg_id
+eeg_spectrogram_count = df.groupby('eeg_id')['spectrogram_id'].nunique()
+
+# Filter the eeg_ids that have more than one unique spectrogram_id
+eeg_ids_with_multiple_spectrogram = eeg_spectrogram_count[eeg_spectrogram_count > 1]
+
+# Display the result
+print(eeg_ids_with_multiple_spectrogram)
+
+
+
+
+# spectrogram_id, spectrogram_sub_id, spectrogram_label_offset_seconds: 
+
+# There are 11138 different spectrogram_id
+# Each spectrogram_id contains spectrogram_sub_id which can go from 0 to 1021 associated to a spectrogram_label_offset_seconds which goes from 0.0 to 17632.0?. Thus the occurency of a spectrogram_id goes from 1 to 1022.
+# A spectrogram_id can contain different eeg_id
+
+
+'''
+# Count the number of unique spectrogram_id
+df_count_spectrogram_id = df['spectrogram_id'].value_counts().reset_index()
+df_count_spectrogram_id.columns = ['spectrogram_id','Count']
+print(df_count_spectrogram_id)
+
+
+# Display if in a figure
+plt.figure(figsize=(12,8))
+plt.bar(df_count_spectrogram_id.index, df_count_spectrogram_id['Count'], color=colors, zorder=2)
+plt.xlabel('spectrogram_id')
+plt.ylabel('Count')
+plt.title('Distribution of spectrogram_id')
+plt.legend()
+plt.grid(ls='--')
+plt.show()
+
+
+# Display the dataframe corresponding to the eeg_id which appears the most 'eeg_id'=2259539799
+df_max_spectrogram_id = df[df['spectrogram_id']==df_count_spectrogram_id.loc[df_count_spectrogram_id['Count'].idxmax(),'spectrogram_id']]
+print(df_max_spectrogram_id)
+
+# Display the different result of expert consensus for this eeg_id
+print(df_max_spectrogram_id['expert_consensus'].unique())
+
+# Display the dataframe corresponding to the eeg_id which appears the least 'eeg_id'=98046913 (one of them)
+df_min_spectrogram_id = df[df['spectrogram_id']==df_count_spectrogram_id.loc[df_count_spectrogram_id['Count'].idxmin(),'spectrogram_id']]
+print(df_min_spectrogram_id)
+
+# Display the different result of expert consensus for this eeg_id
+print(df_min_spectrogram_id['expert_consensus'].unique())
+
+# Display the min-max spectrogram_label_offset_seconds
+print(df['spectrogram_label_offset_seconds'].min())
+print(df['spectrogram_label_offset_seconds'].max())
+'''
+
+
+
+
+# patient_id: There are 1950 different patient_id. The occurrence is between 1 and 2215 corresponding to different combination of ('eeg_id','eeg_sub_id', 'eeg_label_offset_seconds',spectrogram_id',spectrogram_sub_id',spectrogram_label_offset_seconds).
+#                                                  Some combinations appears few times
+'''
+# Count the number of unique Patient_id
+df_count_patient_id = df['patient_id'].value_counts().reset_index()
+df_count_patient_id.columns = ['patient_id','Count']
+print(df_count_patient_id)
+
+# Display if in a figure
+plt.figure(figsize=(12,8))
+plt.bar(df_count_patient_id.index, df_count_patient_id['Count'], color=colors, zorder=2)
+plt.xlabel('patient_id')
+plt.ylabel('Count')
+plt.title('Distribution of patient_id')
+plt.legend()
+plt.grid(ls='--')
+plt.show()
+
+# Display the dataframe corresponding to the patient_id which appears the most ('patient_id'=30631)
+df_max_patient_id = df[df['patient_id']==df_count_patient_id.loc[df_count_patient_id['Count'].idxmax(),'patient_id']]
+print(df_max_patient_id)
+'''
+
+
+
+
+
+
+'''
 # Count the number of unique eeg_id, spectrogram_id, label_id, patient_id and Select columns with float type and observe the histogram
 list_column_id = ['eeg_id', 'spectrogram_id', 'label_id', 'patient_id']
 for id in list_column_id:
@@ -147,6 +286,7 @@ for id in list_column_id:
 print(df_count_id.loc[df_count_id['Count'].idxmax(),'patient_id'])
 
 print(df[df['patient_id']==df_count_id.loc[df_count_id['Count'].idxmax(),'patient_id']])
+'''
 
 '''
 plt.figure(figsize=(12,8))
@@ -161,8 +301,5 @@ for id in list_column_id:
     plt.grid(ls='--')
     plt.show()
 '''
-
-
-print(df['eeg_label_offset_seconds'].unique().sort_values())
 
 
